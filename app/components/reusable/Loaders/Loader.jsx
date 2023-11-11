c5cd444bb57d314842612463e293f9e1b6b80735
Saumyapratim Das <saumyadas2017@gmail.com>
2023-11-11 12:01:10 +0530

    Loader time fix

--- app/components/reusable/Loaders/Loader.jsx
+++ app/components/reusable/Loaders/Loader.jsx
@@ -7,7 +7,7 @@ import styles from './page.module.scss';
 
 const Loader = () => {
     const [index, setIndex] = useState(0);
-    const words = ["Hello", "Namaste", "Guten teg" , "Bonjour", "Ciao", "Guten teg", "Hello"];
+    const words = ["Hello", "Namaste", "Guten teg" , "Bonjour", "Ciao", "Guten teg"];
     const [dimension, setDimension] = useState({width: 0, height:0});
 
     useEffect( () => {
@@ -18,7 +18,7 @@ const Loader = () => {
         if(index == words.length == 1) return;
         setTimeout(() => {
             setIndex(index + 1);
-        }, index == 0 ? 500 : 300)
+        }, index == 0 ? 1000 : 150)
     }, [index]);
 
     const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
